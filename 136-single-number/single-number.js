993b24a076c235f7c515ea430138323d91f83956
Akshay Rawat <155710637+Akshay-Rawat@users.noreply.github.com>
2025-07-31 21:33:06 +0530

    Time: 2 ms (56.25%) | Memory: 56.3 MB (54.80%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -5,8 +5,8 @@
 var singleNumber = function(nums) {
      let ans = 0;
 
-        for(let i=0;i< nums.length;i++){
-            ans = ans^nums[i];
+        for(let i of nums){
+            ans = ans^i;
 
         }
         return ans
