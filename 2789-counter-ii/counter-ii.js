6fd349b8d9b2e31e961556730ea1ac494b3d8719
Akshay Rawat <155710637+Akshay-Rawat@users.noreply.github.com>
2025-09-23 20:18:19 +0530

    Time: 60 ms (8.30%) | Memory: 56 MB (83.71%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let ak=init
+    function increment(){
+        return ++ak
+    }
+    function decrement(){
+        return --ak
+    }
+    function reset(){
+        return (ak=init)
+    }
+    return { increment, decrement, reset }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
