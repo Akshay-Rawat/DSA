cde454deb2978534086c9ebe157a54e14846f0ea
Akshay Rawat <155710637+Akshay-Rawat@users.noreply.github.com>
2025-09-26 13:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (9.89%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,6 +3,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let b = nums.sort()
-    console.log(b)
+   let i = j=0
+   let k = nums.length-1
+
+   while(i<=k){
+    if(nums[i]==0){
+        [nums[i],nums[j]] =  [nums[j],nums[i]]
+        i++,j++
+    }else if(nums[i]==2){
+        [nums[i],nums[k]] =  [nums[k],nums[i]]
+        k--
+    }else{
+        i++
+    }
+   }
 };
