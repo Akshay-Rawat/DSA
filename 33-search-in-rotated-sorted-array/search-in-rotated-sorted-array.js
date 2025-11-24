42ee4750a352283436aaa003dfa2d65a65f45fb5
Akshay Rawat <155710637+Akshay-Rawat@users.noreply.github.com>
2025-11-24 12:46:17 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (59.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,17 +3,20 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let first=0,last=nums.length-1
-    while(first<=last){
-        let mid = Math.floor((first+last)/2)
-        if(nums[mid] == target) return mid;
-        if(nums[first]<=nums[mid]){
-            if(target>=nums[first]&&target<=nums[mid]) last=mid;
-            else first=mid+1;
-        }else{
-            if(target>=nums[mid+1]&&target<=nums[last]) first=mid+1;
-            else last=mid;
+var search = function (nums, target) {
+    let st = 0
+    let end = nums.length -1
+  while(st <= end) {
+        let mid = Math.floor((st+ end) / 2)
+        if (target == nums[mid]) return mid;
+
+        if(nums[st]<=nums[mid]){
+         if (target >= nums[st] && target <= nums[mid])  end = mid ;
+         else st =mid+1
+        }
+        else{
+         if(target>=nums[mid+1] && target<=nums[end]) st=mid+1;
+         else end =mid
         }
     }
     return -1
